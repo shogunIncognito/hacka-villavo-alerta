725ba05485a8f2183fd7ba1c5eff85b80291cc1f
shogunIncognito <saidsamuel2006@hotmail.com>
2024-10-15 16:57:34 -0500

    feat: peticion a openai para conclusion en post

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,10 +1,11 @@
 import { dbConnect } from "@/db/db_config";
 import Post from "@/models/Post";
+import { AIConclusions } from "@/services/api";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     await dbConnect()
-    
+
     const posts = await Post.find({})
     return NextResponse.json(posts)
 }
@@ -13,8 +14,12 @@ export async function POST(req) {
     await dbConnect()
 
     const post = await req.json()
+    const AIPostConclusion = await AIConclusions(post)
     
-    const newPost = new Post(post)
+    const newPost = new Post({
+        ...post,
+        ai_response: AIPostConclusion
+    })
     const savedPost = await newPost.save()
 
     return NextResponse.json(savedPost)
---
+++ src/services/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export async function AIConclusions(post) {
+    const requestToOpenAI = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: 'Esta es una pagina web en la cual se publican noticias de la seguridad de la ciudad de Villavicencio, Colombia. Tu tarea va a ser sacar una conclusion y advertencia/avisos a los usuarios que miren la noticia esten informados y precavidos, te voy a proporcionar el titulo y la descripcion de la noticia. y con esto sacaras la conclusion o advertencia para las entidades o ciudadanos que miren la noticia para que esten informados y alerta. Contesta en Markdown para una mejor visualizacion de textos, El siguiente mensaje sera una noticia real'
+            },
+            {
+                role: 'system',
+                content: `Titulo: ${post.title} - Descripcion: ${post.description}`
+            }
+        ]
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+        }
+    })
+
+    return requestToOpenAI.data.choices[0].message.content
+} 
