61d100b31b5afebd43e24c71ec80ff07bfd241b6
Emerson Roncancio <roncancioavila@gmail.com>
2024-10-15 16:19:22 -0500

    Primer diseño del Navbar y Posts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,23 @@
+import Post from "@/components/Post";
+import postsJson from '@/app/json/posts.json'
+
 export default function Home() {
   return (
     // Vista inicial posts
-    <div>
-      Home
+    <div className="w-full h-full">
+      <main className="">
+        <div className="grid grid-cols-2 place-content-center place-items-center">
+        {
+          postsJson.map(post=>{
+            return(
+              <>
+                <Post key={post._id}/>
+              </>
+            )
+          })
+        }
+        </div>
+      </main>
     </div>
   );
 }
