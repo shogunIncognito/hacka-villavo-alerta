07a4b48b7f4906fe1fd4669521455fac672a9cc4
Emerson Roncancio <roncancioavila@gmail.com>
2024-10-16 16:17:14 -0500

    Skeleton en los posts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,23 @@ import Post from "@/components/Post";
 import { useEffect, useState } from "react";
 import { axiosGet } from "@/helpers/requests/get";
 import { usePosts } from "@/store/posts";
+import { SkeletonPosts } from "@/helpers/helpersAll";
+import { SkeletonCard } from "@/components/SkeletonCard";
 
 export default function Home() {
 
   const { getPosts, setPosts } = usePosts()
+  const [loanding, setLoanding] = useState(false)
 
   useEffect(() => {
+    setLoanding(true)
     axiosGet({ url: '/api/posts' })
       .then(res => {
         const sortedPosts = res.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
         setPosts(sortedPosts);
       })
+      .catch(error => console.log(error))
+      .finally(() => setLoanding(false))
   }, [])
 
   return (
@@ -23,11 +29,18 @@ export default function Home() {
       <main className="">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           {
-            getPosts.map(post => {
-              return (
-                <Post post={post} key={post._id} />
-              )
-            })
+            loanding ?
+              SkeletonPosts.map(skeleton => {
+                return (
+                  <SkeletonCard />
+                )
+              })
+              :
+              getPosts.map(post => {
+                return (
+                  <Post post={post} key={post._id} />
+                )
+              })
           }
         </div>
       </main>
---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+export function SkeletonCard() {
+    return (
+        <div className="flex flex-col space-y-3">
+            <Skeleton className="h-[300px] w-full rounded-xl" />
+            <div className="space-y-2">
+                <Skeleton className="h-8 w-full" />
+                <Skeleton className="h-8 w-full" />
+            </div>
+        </div>
+    )
+}
