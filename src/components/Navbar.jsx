021ac34527f6b98653a77c3eddda48bf21a1d4a5
Miguel <juanmiguelruizsupe@gmail.com>
2024-10-16 08:33:02 -0500

    cambio de boton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import logo from '@/assets/logo.png'
 import Link from 'next/link'
+import { Button } from "@/components/ui/button"
+import { Bell } from "lucide-react"
 
 export default function Navbar() {
 
@@ -10,9 +12,9 @@ export default function Navbar() {
                 <Image src={logo} alt='villavoAlertaslogo' className="object-cover w-48" />
             </Link>
 
-            <Link className='px-4 py-2 rounded-[8px] border border-white flex justify-center items-center' href='/'>
-                <span className='text-white'>SUSCRIBETE</span>
-            </Link>
+            <Button variant="outline" className="bg-primary text-white" >
+                <Bell className="mr-2 h-4 w-4" /> Suscríbete
+            </Button>
         </header>
     )
 }
