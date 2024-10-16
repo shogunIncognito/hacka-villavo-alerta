daa84bf34457f46fbd9cea9e68e1ebba86cdbeaf
shogunIncognito <saidsamuel2006@hotmail.com>
2024-10-15 23:24:01 -0500

    feat: mejorado diseño de login admin

--- src/app/auth/admin/page.jsx
+++ src/app/auth/admin/page.jsx
@@ -15,13 +15,10 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+
+import logo from '@/assets/logo.png'
+import loginBackground from '@/assets/login-background.png'
+import Image from 'next/image'
 
 export default function Login() {
   const router = useRouter()
@@ -35,54 +32,40 @@ export default function Login() {
       .then((res) => {
         if (!((res?.ok) ?? false)) return console.log('Error al iniciar sesión')
         // toast.success('Inicio de sesión exitoso.')
-        router.replace('/dash')
+        router.replace('/')
       })
-
   }
 
   return (
     <div className='flex justify-center items-center h-full w-full flex-1'>
-      <form onSubmit={handleLogin} className="bg-black/50 h-full flex items-center justify-center p-5 gap-2">
-        <input type="text" name='email' />
-        <input type="password" name='password' />
-
-        <button>Iniciar sesion</button>
-      </form>
-
-        <Card className="w-[350px]">
+      <Card className="w-[400px] shadow-lg">
+        <form onSubmit={handleLogin}>
           <CardHeader>
-            <CardTitle>Create project</CardTitle>
-            <CardDescription>Deploy your new project in one-click.</CardDescription>
+            <Image alt='villavoAlertaslogo' src={logo} className="object-cover invert w-68 h-28" />
+            <CardTitle className='text-center text-2xl'>Iniciar Sesión</CardTitle>
+            <CardDescription>Accede a tu cuenta para recibir alertas personalizadas. Ingresa tus credenciales y haz clic en "Iniciar Sesión".</CardDescription>
           </CardHeader>
           <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" placeholder="Name of your project" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="framework">Framework</Label>
-                  <Select>
-                    <SelectTrigger id="framework">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">Next.js</SelectItem>
-                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                      <SelectItem value="astro">Astro</SelectItem>
-                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label className='font-bold' htmlFor="email">Correo</Label>
+                <Input id="email" placeholder="example@gmail.com" />
               </div>
-            </form>
+              <div className="flex flex-col space-y-1.5">
+                <Label className='font-bold' htmlFor="password">Contraseña</Label>
+                <Input id="password" type="password" placeholder="******" />
+              </div>
+            </div>
           </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Deploy</Button>
+          <CardFooter className="flex justify-center">
+            <Button>Iniciar sesion</Button>
           </CardFooter>
-        </Card>
+        </form>
+      </Card>
+
+      <div className="absolute top-0 opacity-10 right-0 bottom-0 left-0 z-[-1]">
+        <Image src={loginBackground} layout="fill" alt='background-login' />
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import logo from '@/app/fonts/logo.png'
-import { buttonVariants } from "@/components/ui/button"
+import logo from '@/assets/logo.png'
 import Link from 'next/link'
 
 export default function Navbar() {
@@ -8,7 +7,7 @@ export default function Navbar() {
     return (
         <header className="w-full h-[70px] bg-[#2563eb] flex justify-between items-center px-4">
             <Link href='/'>
-                <Image src={logo} className="object-cover w-48" />
+                <Image src={logo} alt='villavoAlertaslogo' className="object-cover w-48" />
             </Link>
 
             <Link className='px-4 py-2 rounded-[8px] border border-white flex justify-center items-center' href='/'>
