4514e8b908880f1b42424d59bcb16ffa149ba630
Emerson Roncancio <roncancioavila@gmail.com>
2024-10-15 21:09:00 -0500

    preparacion de la ruta de creacion de alertas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,15 +3,16 @@
 import Post from "@/components/Post";
 import { useEffect, useState } from "react";
 import { axiosGet } from "@/helpers/requests/get";
+import { usePosts } from "@/context/posts";
 
 export default function Home() {
 
-  const [getPosts, setgetPosts] = useState([])
+  const {getPosts, setPosts} = usePosts()
 
   useEffect(() => {
     axiosGet({url:'/api/posts'})
-      .then(res=> setgetPosts(res))
-  }, [getPosts])
+      .then(res=> setPosts(res))
+  }, [])
   
 
   return (
