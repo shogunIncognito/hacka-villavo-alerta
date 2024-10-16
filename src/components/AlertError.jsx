26865e13c84ff092a51edac369a8f2b0cba83746
Emerson Roncancio <roncancioavila@gmail.com>
2024-10-16 15:20:47 -0500

    alerts components y manejo de errores en suscribirse

---
+++ src/components/AlertError.jsx
@@ -0,0 +1,21 @@
+import { AlertCircle } from "lucide-react"
+
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
+
+export function AlertError({ description }) {
+    return (
+        <div className="flex justify-center items-center top-0 py-5 w-full absolute">
+            <Alert className="z-[100] w-[50%] bg-destructive">
+                <AlertCircle color="white" className="h-4 w-4" />
+                <AlertTitle><span className="text-white">Error</span></AlertTitle>
+                <AlertDescription>
+                    <span className="text-white">{description}</span>
+                </AlertDescription>
+            </Alert>
+        </div>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,26 +17,41 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { AlertError } from './AlertError';
 
 export default function Navbar() {
     const [email, setEmail] = useState('');
+    const [alertError, setAlertError] = useState(false)
+    const [description, setDescription] = useState('')
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+    const onSubmit = (e) => {
+        e.preventDefault();
 
-    const handleSubscribe = () => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (emailRegex.test(email)) {
-            console.log('Correo suscrito:', email);
-            // Aquí puedes agregar la lógica para enviar el correo a una API o backend
-        } else {
-            console.log('Por favor, ingresa un correo válido.');
+        if (email.length <= 0) {
+            setAlertError(true)
+            setDescription('El correo es requerido')
+            setTimeout(() => {
+                setAlertError(false)
+            }, 5000)
+            return
+        } else if (!emailRegex.test(email)) {
+            setAlertError(true)
+            setDescription('Tienes que ingresar un correo valido')
+            setTimeout(() => {
+                setAlertError(false)
+            }, 5000)
+            return
         }
-    };
+
+        setEmail('')
+    }
 
     return (
         <header className="w-full h-[70px] bg-[#2563eb] flex justify-between items-center px-4">
+            {
+                alertError ? <AlertError description={description} /> : null
+            }
             <div className="flex items-center">
                 <Link href='/'>
                     <Image src={logo} alt='villavoAlertaslogo' className="object-cover w-48" />
@@ -55,36 +70,38 @@ export default function Navbar() {
                         </Button>
                     </DialogTrigger>
                     <DialogContent className="w-[600px]">
-                        <DialogHeader>
-                            <DialogTitle className="text-center">Suscríbete</DialogTitle>
-                            <DialogDescription>
-                                Recibe notificaciones cada vez que subamos una nueva alerta. Haz clic en guardar cuando hayas terminado.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="email" className="text-right">
-                                    Correo
-                                </Label>
-                                <Input
-                                    id="email"
-                                    value={email}
-                                    onChange={handleEmailChange}
-                                    className="col-span-3"
-                                    placeholder="Ingresa tu correo"
-                                />
+                        <form onSubmit={onSubmit}>
+                            <DialogHeader>
+                                <DialogTitle className="text-center">Suscríbete</DialogTitle>
+                                <DialogDescription>
+                                    Recibe notificaciones cada vez que subamos una nueva alerta. Haz clic en guardar cuando hayas terminado.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div className="grid gap-4 py-4">
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="email" className="text-right">
+                                        Correo
+                                    </Label>
+                                    <Input
+                                        id="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        className="col-span-3"
+                                        placeholder="Ingresa tu correo"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <DialogFooter className="">
-                            <div className='flex w-full justify-between'>
-                                <Link href='https://villavicencio.gov.co/' aria-label="Alcaldia Website" target="_blank">
-                                    <Image src={logo} alt='Alcaldia Logo' className="object-cover invert w-48 h-[50px]" />
-                                </Link>
-                                <Button onClick={handleSubscribe} className="bg-primary text-center">
-                                    Guardar
-                                </Button>
-                            </div>
-                        </DialogFooter>
+                            <DialogFooter className="">
+                                <div className='flex w-full justify-between'>
+                                    <Link href='https://villavicencio.gov.co/' aria-label="Alcaldia Website" target="_blank">
+                                        <Image src={logo} alt='Alcaldia Logo' className="object-cover invert w-48 h-[50px]" />
+                                    </Link>
+                                    <Button className="bg-primary text-center">
+                                        Guardar
+                                    </Button>
+                                </div>
+                            </DialogFooter>
+                        </form>
                     </DialogContent>
                 </Dialog>
             </div>
