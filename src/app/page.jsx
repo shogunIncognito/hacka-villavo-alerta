dd6a8092c49362f9a0d584bead1eb547eb6be79e
shogunIncognito <saidsamuel2006@hotmail.com>
2024-10-17 14:54:23 -0500

    feat: paginacion en posts client side

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,28 +3,39 @@
 import Post from "@/components/Post";
 import { useEffect, useState } from "react";
 import { axiosGet } from "@/helpers/requests/get";
-import { usePosts } from "@/store/posts";
 import { SkeletonPosts } from "@/helpers/helpersAll";
 import { SkeletonCard } from "@/components/SkeletonCard";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
-
-  const { getPosts, setPosts } = usePosts()
+  const [page, setPage] = useState(1)
+  const [posts, setPosts] = useState({
+    posts: [],
+    lastPage: false
+  })
   const [loading, setLoading] = useState(true)
+  const [pageLoading, setPageLoading] = useState(false)
 
   useEffect(() => {
-    axiosGet({ url: '/api/posts' })
+    axiosGet({ url: `/api/posts?page=${page}` })
       .then(res => {
-        setPosts(res.posts);
+        setPosts({
+          posts: [...posts.posts, ...res.posts],
+          lastPage: res.lastPage
+        });
       })
       .catch(error => console.log(error))
-      .finally(() => setLoading(false))
-  }, [])
+      .finally(() => {
+        setLoading(false)
+        setPageLoading(false)
+      })
+  }, [page])
 
   return (
     // Vista inicial posts
     <div className="w-full h-full p-11">
-      <main className="">
+      <main>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           {
             loading ?
@@ -34,13 +45,29 @@ export default function Home() {
                 )
               })
               :
-              getPosts.map(post => {
+              posts.posts.map(post => {
                 return (
                   <Post post={post} key={post._id} />
                 )
               })
           }
+
         </div>
+
+        {!posts.lastPage && (
+          <div className="flex justify-center mt-10">
+            <Button
+              onClick={() => {
+                setPageLoading(true)
+                setPage(page + 1)
+              }}
+              disabled={pageLoading}
+              className="md:w-1/6 w-1/3 shadow" variant='ghost'
+            >
+              {pageLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : 'Cargar más'}
+            </Button>
+          </div>
+        )}
       </main>
     </div>
   );
